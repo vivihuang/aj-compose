20c70348903623d1d6b976108beca9f5c11fc02b
mutoe <mutoe@foxmail.com>
2019-11-13 15:29:50 +0800

    [TECH] fix start-all.sh update will be ran twice

--- start-all.sh
+++ start-all.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 
+build_gradlew() {
+  echo "==== Build all images before start ===="
+  cd ../aj-georgia
+  ./gradlew build
+  cd ../aj-latvia
+  ./gradlew build
+  cd ../aj-russia
+  docker-compose build
+}
+
 case $1 in
   build)
-    echo "==== Build all images before start ===="
-    cd ../aj-georgia
-    ./gradlew build
-    cd ../aj-latvia
-    ./gradlew build
-    cd ../aj-russia
-    docker-compose build
+    build_gradlew
     ;;
   update)
     echo "==== Update and build ===="
     ./pull-all.sh
-    ./start-all.sh build
+    build_gradlew
     ;;
 esac
 
+echo -e "\n==== ☢️  The nuclear is preparing.... ===="
 docker-compose up
 
+echo -e "\n==== 🚿 Revoming all services... please do NOT Ctrl-C again ===="
 docker-compose down
+
+echo -e "\n==== 🎉 Congratulations on getting a clean computer! ===="
